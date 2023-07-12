ad7f4b9244e18c8abcdabd5265bf0859d7c07a44
ilido <danilashapovalov8@gmail.com>
2023-07-12 23:31:53 +0400

    add_tgui_template_for_console

---
+++ tgui/packages/tgui/interfaces/Helm.tsx
@@ -0,0 +1,12 @@
+import { Box } from "../components";
+import { Window } from "../layouts";
+export const Helm = (props, context) => {
+  return (
+
+    <Window>
+      <Window.Content>
+        <Box>Hello, helm console</Box>
+      </Window.Content>
+    </Window>
+    );
+};
